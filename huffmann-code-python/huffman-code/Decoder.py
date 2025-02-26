526a511d86222bdec92c064c8a1c313da0273953
x-kiwi199 <vivianbecher@web.de>
2025-02-26 11:24:41 +0100

    Initial blueprint for a encoder/decoder class implementation solution

---
+++ huffmann-code-python/huffman-code/Decoder.py
@@ -0,0 +1,53 @@
+# *     Project: huffman-code-python
+# *      Module: huffman-code
+# *      Script: Decoder.py
+# * Description: Forward-Error-Correction (FEC) decoder, system module abstraction of communication link, codeword (input) -> input (output)
+# *
+# *  Created on: Feb 24, 2025
+# *      Author: Vivian Becher
+# *     Contact: x-kiwi199@web.de
+# *     License: [The Unlicense]
+# -----------------------------------------------------------------------
+# This is free and unencumbered software released into the public domain.
+#
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
+#
+# In jurisdictions that recognize copyright laws, the author or authors
+# of this software dedicate any and all copyright interest in the
+# software to the public domain. We make this dedication for the benefit
+# of the public at large and to the detriment of our heirs and
+# successors. We intend this dedication to be an overt act of
+# relinquishment in perpetuity of all present and future rights to this
+# software under copyright law.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# For more information, please refer to <https://unlicense.org/>
+# -----------------------------------------------------------------------
+
+import Codebook
+
+class Decoder:
+    def __init__(self):
+        self.codebook = None
+
+        self.codeword = None
+        self.message = None
+        pass
+
+    def decode(self, codeword:str):
+        self.codeword = self.codeword
+        self.message = codeword
+        return self.message
+
+    def initialize(self):
+        pass
---
+++ huffmann-code-python/huffman-code/__init__.py
@@ -0,0 +1,5 @@
+from .Encoder import Encoder
+from .Decoder import Decoder
+from .Codebook import Codebook
+
+__all__ = ['Encoder', 'Decoder', 'Codebook']
